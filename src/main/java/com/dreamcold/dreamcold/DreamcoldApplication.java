2b771dfd40fe64db8f1edf0e79cb61177f802779
super.kang <1689242311@qq.com>
2020-07-28 23:16:05 +0800

    项目开始进行

---
+++ src/main/java/com/dreamcold/dreamcold/DreamcoldApplication.java
@@ -0,0 +1,13 @@
+package com.dreamcold.dreamcold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamcoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamcoldApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/dreamcold/dreamcold/controller/AssetPricingController.java
@@ -0,0 +1,8 @@
+package com.dreamcold.dreamcold.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AssetPricingController {
+}
